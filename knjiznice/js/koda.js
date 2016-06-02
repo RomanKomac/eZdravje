6bb47509bab9d0df29c0db04432bf2b5690961f5
Roman Komac <rk3367@student.uni-lj.si>
2016-06-02 19:15:30 +0000

    Added cookie library, implementing data generation

--- knjiznice/js/koda.js
+++ knjiznice/js/koda.js
@@ -5,6 +5,21 @@ var queryUrl = baseUrl + '/query';
 var username = "ois.seminar";
 var password = "ois4fri";
 
+var pacientiName      = ["Zdravka", "Marija", "Tija"];
+var pacientiSurname   = ["Dren", "Novak", "Suhe"];
+var pacientiBirthDate = ["1988-12-09T00:00:00.000Z", "1978-02-12T00:00:00.000Z", "1990-04-02T00:00:00.000Z"];
+var pacientiWeight    = ["67", "72", "50"];
+var pacientiHeight    = ["169", "165", "174"];
+var pacientiSistolycPressure = [
+        
+        
+    ];
+
+var pacientiDiastolycPressure = [
+        
+        
+    ];
+
 
 /**
  * Prijava v sistem z privzetim uporabnikom za predmet OIS in pridobitev
@@ -33,10 +48,59 @@ function getSessionId() {
 function generirajPodatke(stPacienta) {
   ehrId = "";
 
-  // TODO: Potrebno implementirati
+    ehrId = kreirajEHR(stPacienta);
 
   return ehrId;
 }
 
+function generirajPodatkePacientov(){
+    for(var i = 1; i <= 3; i++)
+        generirajPodatke(i);
+}
 
-// TODO: Tukaj implementirate funkcionalnost, ki jo podpira vaša aplikacija
+$(document).ready(function() {
+
+});
+
+
+
+function kreirajEHR(stPacienta) {
+	sessionId = getSessionId();
+
+		$.ajaxSetup({
+		    headers: {"Ehr-Session": sessionId}
+		});
+		$.ajax({
+		    url: baseUrl + "/ehr",
+		    type: 'POST',
+		    success: function (data) {
+		        var ehrId = data.ehrId;
+		        var partyData = {
+		            firstNames: ime,
+		            lastNames: priimek,
+		            dateOfBirth: datumRojstva,
+		            partyAdditionalInfo: [{key: "ehrId", value: ehrId}]
+		        };
+		        $.ajax({
+		            url: baseUrl + "/demographics/party",
+		            type: 'POST',
+		            contentType: 'application/json',
+		            data: JSON.stringify(partyData),
+		            success: function (party) {
+		                if (party.action == 'CREATE') {
+		                    $("#kreirajSporocilo").html("<span class='obvestilo " +
+                          "label label-success fade-in'>Uspešno kreiran EHR '" +
+                          ehrId + "'.</span>");
+		                    $("#preberiEHRid").val(ehrId);
+		                }
+		            },
+		            error: function(err) {
+		            	$("#kreirajSporocilo").html("<span class='obvestilo label " +
+                    "label-danger fade-in'>Napaka '" +
+                    JSON.parse(err.responseText).userMessage + "'!");
+		            }
+		        });
+		    }
+		});
+	
+}
