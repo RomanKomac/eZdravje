75443f98560686be5c3f3ac4e31918ff46897bf7
Roman Komac <rk3367@student.uni-lj.si>
2016-06-02 22:38:28 +0000

    Implemented session, logout and data generation(still some fixes needed)

--- knjiznice/js/koda.js
+++ knjiznice/js/koda.js
@@ -5,6 +5,8 @@ var queryUrl = baseUrl + '/query';
 var username = "ois.seminar";
 var password = "ois4fri";
 
+var pacienti = [];
+
 var pacientiName      = ["Zdravka", "Marija", "Tija"];
 var pacientiSurname   = ["Dren", "Novak", "Suhe"];
 var pacientiBirthDate = ["1988-12-09T00:00:00.000Z", "1978-02-12T00:00:00.000Z", "1990-04-02T00:00:00.000Z"];
@@ -46,33 +48,79 @@ function getSessionId() {
  * @return ehrId generiranega pacienta
  */
 function generirajPodatke(stPacienta) {
-  ehrId = "";
+  var ehrId = "";
 
     ehrId = kreirajEHR(stPacienta);
+            //kreirajData(stPacienta);
+
 
   return ehrId;
 }
 
 function generirajPodatkePacientov(){
-    for(var i = 1; i <= 3; i++)
+    for(var i = 0; i < 3; i++)
         generirajPodatke(i);
+    //skrije background
+    Cookies.set("Session", "generirano");
+    $("#background").hide();
+    setTimeout(function(){
+        $("#message").html("");
+    }, 4000)
 }
 
 $(document).ready(function() {
-    if(!Cookies.get('name')){
-        Cookies.set('name', 'value');
-        console.log("Setting cookie");
-    } else {
-        console.log("cookie already set");
-        
+    var sess = Cookies.get('Session');
+    if(sess && sess != "generirano"){
+        $("#background").hide();
     }
+    
+    $("#EhrInput").keyup(function(event) {
+        if (event.keyCode == 13) {
+            var pacient = preberiEHR($("#EhrInput").val());
+         }
+    });
+    
+    $("#logout").click(function(event) {
+        pacienti = [];
+        Cookies.remove("Session");
+        location.reload();
+    });
+    
 });
 
 
-/*
-function kreirajEHR(stPacienta) {
-	sessionId = getSessionId();
+function preberiEHR(ehrId) {
+	var sessionId = getSessionId();
 
+	if (!ehrId || ehrId.trim().length == 0) {
+		$("#message").html("<span class='obvestilo label label-warning " +
+      "fade-in'>Prosim vnesite veljaven Ehr-ID!");
+	} else {
+		$.ajax({
+			url: baseUrl + "/demographics/ehr/" + ehrId + "/party",
+			type: 'GET',
+			headers: {"Ehr-Session": sessionId},
+	    	success: function (data) {
+	    	    $("#message").html("");
+	    	    Cookies.set("Session", "vneseno");
+	    	    $("#background").hide();
+				var party = data.party;
+				pacienti.push(party);
+				console.log(party);
+				return party;
+			},
+			error: function(err) {
+				$("#message").html("<span class='obvestilo label " +
+          "label-danger fade-in'>Napaka '" +
+          JSON.parse(err.responseText).userMessage + "'!");
+			}
+		});
+	}
+}
+
+
+function kreirajEHR(stPacienta) {
+	var sessionId = getSessionId();
 		$.ajaxSetup({
 		    headers: {"Ehr-Session": sessionId}
 		});
@@ -82,9 +130,9 @@ function kreirajEHR(stPacienta) {
 		    success: function (data) {
 		        var ehrId = data.ehrId;
 		        var partyData = {
-		            firstNames: ime,
-		            lastNames: priimek,
-		            dateOfBirth: datumRojstva,
+		            firstNames: pacientiName[stPacienta],
+		            lastNames: pacientiSurname[stPacienta],
+		            dateOfBirth: pacientiBirthDate[stPacienta],
 		            partyAdditionalInfo: [{key: "ehrId", value: ehrId}]
 		        };
 		        $.ajax({
@@ -94,19 +142,23 @@ function kreirajEHR(stPacienta) {
 		            data: JSON.stringify(partyData),
 		            success: function (party) {
 		                if (party.action == 'CREATE') {
-		                    $("#kreirajSporocilo").html("<span class='obvestilo " +
+		                    $("#message").html($("#message")[0].innerHTML+"<span class='obvestilo " +
                           "label label-success fade-in'>Uspešno kreiran EHR '" +
                           ehrId + "'.</span>");
-		                    $("#preberiEHRid").val(ehrId);
+		                    //$("#preberiEHRid").val(ehrId);
+		                    pacienti.push(partyData);
+		                    console.log(partyData)
+		                    return ehrId;
 		                }
 		            },
 		            error: function(err) {
-		            	$("#kreirajSporocilo").html("<span class='obvestilo label " +
+		            	$("#message").html("<span class='obvestilo label " +
                     "label-danger fade-in'>Napaka '" +
                     JSON.parse(err.responseText).userMessage + "'!");
+                    return null;
 		            }
 		        });
 		    }
 		});
 	
-}*/
+}
